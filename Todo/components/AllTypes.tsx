40d7cfe648f3accf0f2bc93ad40c7d8f07d3f7c2
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-16 23:56:38 +0530

    label created for all types of todos and for gradient effect on the scroll, used expo-linear-gradient

--- Todo/components/AllTypes.tsx
+++ Todo/components/AllTypes.tsx
@@ -1,9 +1,8 @@
 import { View, Text, FlatList, Pressable} from 'react-native'
 import {LinearGradient} from 'expo-linear-gradient'
 import {useColorScheme} from 'nativewind'
-
-
 import { Alltabs } from '@/constants/data'
+
 
 const AllTypes = () => {
 
@@ -12,6 +11,8 @@ const AllTypes = () => {
   const bg=colorScheme==="dark"?"#09090b":"#ffffff"
 
   const transparent =colorScheme==="dark"?"rgba(9,9,11,0)":"rgba(255,255,255,0)"
+
+
   return (
     <View className=' relative'>
       <FlatList
@@ -20,15 +21,12 @@ const AllTypes = () => {
       horizontal
       showsHorizontalScrollIndicator={false}
       renderItem={({item})=>(
+
       <Pressable
        className='px-4 py-2 rounded-xl bg-neutral-200 active:bg-neutral-300 m-2 dark:bg-neutral-700 dark:active:bg-neutral-800'>
           <Text className='text-md text-neutral-800 dark:text-neutral-100'>{item}</Text>
-
       </Pressable>
-    )}
-    >
-
-      </FlatList>
+        )}/>
 
       <LinearGradient
       pointerEvents='none'
@@ -40,8 +38,7 @@ const AllTypes = () => {
         left:0,
         top:0,
         bottom:0,
-        width:6,
-        filter:"blur(1px)"
+        width:6
       }}/>
 
       <LinearGradient
@@ -54,13 +51,10 @@ const AllTypes = () => {
         right:0,
         top:0,
         bottom:0,
-        width:6,
-        filter:"blur(1px)"
+        width:6
       }}/>
 
   
-
-      
     </View>
   )
 }
