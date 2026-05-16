ebaa6a52852ea543af07015a9dd1250403b21459
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-17 00:00:08 +0530

    some new data added and type created for ts

--- Todo/constants/data.ts
+++ Todo/constants/data.ts
@@ -1,3 +1,10 @@
 export const Alltabs=[
   "All","In progress","Overdue","Today","Next 7 days","High priority"
 ]
+
+export const tabsSelectColors = [
+  { color: "#ef4444", bgColor: "#fee2e2" ,textColor:"#E8A7B1",btnColor:"#DE6A9B"},
+  { color: "#3b82f6", bgColor: "#dbeafe" ,textColor:"#7477A6",btnColor:"#4168A3"},
+  { color: "#22c55e", bgColor: "#dcfce7" ,textColor:"#5FA35B",btnColor:"#8AE685"},
+  { color: "#8b5cf6", bgColor: "#ede9fe" ,textColor:"#AF6BB3",btnColor:"#C353C9"},
+]
--- Todo/type.d.ts
+++ Todo/type.d.ts
@@ -3,6 +3,23 @@ import React from "react";
 
 declare global{
 
+  interface newTodoProps{
+    isOpen:boolean;
+    onClose:()=>void
+  }
+
+  interface AllTodos{
+    id:string;
+    todo:string;
+    date:string;
+  }
+
+  interface AllColors{
+    color:string;
+    bgColor:string;
+    textColor:string;
+    btnColor:string
+  }
   
 }
 export {}
