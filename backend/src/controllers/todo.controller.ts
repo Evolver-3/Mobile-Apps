e395dc43e4450b9b84fb0f103c89a794154f415f
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-19 23:58:20 +0530

    controller and routes for creating a todo, getting todos according to tags, getting todo by id, delete by id added

--- backend/src/controllers/todo.controller.ts
+++ backend/src/controllers/todo.controller.ts
@@ -4,15 +4,77 @@ import ApiError from '../utils/ApiError'
 import ApiResponse from '../utils/ApiResponse'
 
 export const createNewTodo=asyncHandler(async(req,res)=>{
-  const {title,tag}=req.body
+  const {title,tag,colors}=req.body
 
-  if(!title){
+  if(!title?.trim()){
     throw new ApiError(400, "Title is required")
   }
 
   const todo=await prisma.todo.create({
-    data:{title,tag}
+    data:{title,tag,colors}
   })
 
   return res.status(201).json(new ApiResponse(201,todo,"Todo created"))
+})
+
+
+export const getTodo=asyncHandler(async(req,res)=>{
+  const {id}=req.params
+
+  if(!id){
+    throw new ApiError(400, "id is required")
+  }
+    const todo=await prisma.todo.findUnique({
+      where:{
+        id
+      }
+    })
+
+    if(!todo){
+      throw new ApiError(404,"Todo not found")
+    }
+
+    return res.status(200).json(new ApiResponse(200,todo,"Get todo by Id"))
+})
+
+export const deleteTodo=asyncHandler(async(req,res)=>{
+
+  const {id}=req.params
+
+  if(!id){
+    throw new ApiError(400, "id is required")
+  }
+
+  const todo=await prisma.todo.delete({
+    where:{
+      id,
+    }
+  })
+
+  return res.status(200).json(new ApiResponse(200,todo,"Todo deleted"))
+})
+
+export const getTodoByTag=asyncHandler(async(req,res)=>{
+
+  const {tag}=req.params
+
+  if(!tag){
+    throw new ApiError(402,"Tag is required")
+  }
+
+  const todo=await prisma.todo.findMany({
+    where:{
+      tag
+    }
+  })
+
+  if(!todo){
+    throw new ApiError(404,"No todo for this particular exist")
+  }
+
+  return res.status(200).json(new ApiResponse(200, todo, "Todo according to the tag"))
+})
+
+export const updateTodo=asyncHandler(async(req,res)=>{
+  
 })
--- backend/src/routes/todo.routes.ts
+++ backend/src/routes/todo.routes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { createNewTodo } from "../controllers/todo.controller";
+import { createNewTodo, deleteTodo, getTodo ,getTodoByTag,updateTodo} from "../controllers/todo.controller";
 
 
 const router=Router()
 
 router.route("/newTodo").post(createNewTodo)
+
+router.route("/tag/:tag").get(getTodoByTag)
+
+router.route("/:id").get(getTodo)
+
+router.route("/delete/:id").delete(deleteTodo)
+
+router.route("/update/:id").patch(updateTodo)
+
 
 export default router
