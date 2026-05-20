4845498926ac678ab8e5e4be501629258acc9e5e
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-21 00:48:59 +0530

    a hook page for essential hooks like creating a new todo and fetching todos according to their tag

---
+++ Todo/hooks/useTodos.ts
@@ -0,0 +1,55 @@
+import { createTodo ,getTodoByTag} from "@/services/todoServices";
+import { useEffect, useState } from "react";
+
+
+export const useTodos=(selectedTab:string)=>{
+  const [todos,setTodos]=useState<AllTodos[]>([])
+  const [loading,setLoading]=useState(false)
+
+ const fetchTodos=async()=>{
+  try{
+    setLoading(true)
+
+    const res=await getTodoByTag(selectedTab)
+
+    setTodos(res.data)
+  }catch(error){
+    console.log(error)
+  }finally{
+    setLoading(false)
+  }
+ }
+
+
+ const createNewTodos=async(title:string,colors:string)=>{
+  try{
+    setLoading(true)
+
+    if(!selectedTab){
+      throw new Error("Tag is required")
+    }
+
+    const res=await createTodo({title,tag:selectedTab,colors})
+
+    return res
+  }catch(error){
+    console.log(error)
+  }finally{
+    setLoading(false)
+  }
+ }
+
+  useEffect(()=>{
+    if(selectedTab){
+      fetchTodos()
+    }
+  },[selectedTab])
+
+  return{
+    todos,
+    loading,
+    setTodos,
+    fetchTodos,
+    createNewTodos
+  }
+}
