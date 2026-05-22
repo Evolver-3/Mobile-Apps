70adbf72cf67f3dcd758c3168b1fcdfe06b52fc4
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-22 23:31:29 +0530

    working todo list shows here, also with a checkbox when clicked changed the todo to completed todo which makes it automatically goes to bottom. Also a delete todo button added which shows when a todo is swiped left

--- Todo/components/CreatedTodo.tsx
+++ Todo/components/CreatedTodo.tsx
@@ -1,26 +1,56 @@
-import { View, Text ,FlatList, Pressable} from 'react-native'
-import React, { useEffect } from 'react'
-import { CheckCircle, CheckCircleIcon,BoxSelectIcon} from 'lucide-react-native'
+import { View, Text ,FlatList, Pressable,ScrollView} from 'react-native'
+import {useState} from 'react'
+import { CircleCheck,Circle, ChevronUp, Trash2,} from 'lucide-react-native'
+import ReanimatedSwipeable from 'react-native-gesture-handler/ReanimatedSwipeable'
 
 
 type createdTodoProps={
   todo:AllTodos[],
   selectedTab:string,
-  changeStatus:(id:string)=>Promise<AllTodos|undefined>
+  changeStatus:(id:string)=>Promise<AllTodos|undefined>,
+  deleteSelectedTodo:(id:string)=>Promise<void>
 }
 
-const CreatedTodo = ({todo,selectedTab,changeStatus}:createdTodoProps) => {
+const CreatedTodo = ({todo,selectedTab,changeStatus,deleteSelectedTodo}:createdTodoProps) => {
 
   const filteredData=selectedTab==="All"?todo:todo.filter(
     (item)=>item.tag===selectedTab
   )
 
+  const incompleteTodo=filteredData.filter((item)=>!item.completed)
+
+  const completeTodo=filteredData.filter((item)=>item.completed)
+
+  const [allcompleteTodo,setCompleteTodo]=useState(false)
+
+  const completeData=allcompleteTodo?completeTodo.slice(0,1):completeTodo
+
+  
+
   return (
-    <FlatList
-    data={filteredData}
+    <ScrollView className='flex-1'
+    showsVerticalScrollIndicator={false}>
+      <FlatList
+    data={incompleteTodo}
     keyExtractor={(item)=>item.id}
-    renderItem={({item})=>(
-      <View className='p-4 rounded-xl m-2 justify-between flex-row'
+    scrollEnabled={false}
+    renderItem={({item})=>{
+    
+    const RightActions=()=>(
+    <Pressable
+    onPress={()=>deleteSelectedTodo(item.id)}>
+      <View className='rounded-xl bg-red-200 p-4 m-2 flex-1'>
+      <Trash2 size={30}/>
+
+      </View>
+
+    </Pressable>)
+
+      return(
+      <ReanimatedSwipeable
+      renderRightActions={RightActions}
+      >
+        <View className='p-4 rounded-xl m-2 justify-between flex-row'
       style={{
         backgroundColor:item.colors 
       }}>
@@ -31,20 +61,82 @@ const CreatedTodo = ({todo,selectedTab,changeStatus}:createdTodoProps) => {
         <Pressable
         className='w-10 h-10 items-center justify-center'
         onPress={()=>{
-          changeStatus(item.id)
-          console.log(item.completed)
-          console.log(item.id)}}>
+          changeStatus(item.id)}}>
 
           {item.completed?(
-            <CheckCircle size={24}/>):(
-              <BoxSelectIcon size={24}/>
+            <CircleCheck size={24}/>):(
+              <Circle size={24}/>
             )}
 
         </Pressable>
       </View>
-    )}
-    />
+      </ReanimatedSwipeable>
+      )}}/>
+
+     <FlatList
+    data={completeData}
+    keyExtractor={(item)=>item.id}
+    scrollEnabled={false}
+    ListHeaderComponent={
+      completeTodo.length>0?(
+        <View className='flex-row justify-between px-4 mt-4'>
+          <Text className='text-white dark:text-neutral-200  text-md'>Done</Text>
+          <View className="flex-row items-center gap-2">
+            <Text className="text-md text-black dark:text-white">{completeTodo.length>1?completeTodo.length:null}</Text>
+
+            <Pressable
+            onPress={()=>setCompleteTodo((prev)=>!prev)}
+            style={{
+                transform:[
+                  {rotate:completeTodo?"180deg":"0deg"}
+                ]
+               }}>
+               <ChevronUp size={16}
+               
+                />
+            </Pressable>
+          </View>
+        </View>
+      ):null
+    }
+    renderItem={({item})=>{
+      const RightActions=()=>(
+    <Pressable
+    onPress={()=>deleteSelectedTodo(item.id)}>
+      <View className='rounded-xl bg-red-200 p-4 m-2 flex-1'>
+      <Trash2 size={30}/>
+
+      </View>
+
+    </Pressable>)
+
+    return(
+      <ReanimatedSwipeable renderRightActions={RightActions}>
+        <View className='p-4 rounded-2xl m-2 justify-between flex-row '
+      style={{
+        backgroundColor:"#D9D2D9" 
+      }}>
+        <View className="flex-col gap-y-3 ">
+ 
+        <Text className="text-neutral-600 dark:text-neutral-700">{item.title}</Text>
+        </View>
+        <Pressable
+        className='w-10 h-10 items-center justify-center '
+        onPress={()=>{
+          changeStatus(item.id)}}>
+
+          {item.completed?(
+            <CircleCheck size={24}/>):(
+              <Circle size={24}/>
+            )}
+
+        </Pressable>
+      </View>
+      </ReanimatedSwipeable>
+    )}}/>
+    </ScrollView>
   )
 }
 
 export default CreatedTodo
+
