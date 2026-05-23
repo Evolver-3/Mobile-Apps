01ba00d95e29484c137c40be78c3ed7e298ee8e3
Ashu Singh <Ashusingh2201@gmail.com>
2026-05-24 02:12:31 +0530

    updated color data

--- Todo/constants/data.ts
+++ Todo/constants/data.ts
@@ -1,10 +1,18 @@
-export const Alltabs=[
+export const Alltabs:TodoTag[]=[
   "All","In progress","Overdue","Today","Next 7 days","High priority"
 ]
 
 export const tabsSelectColors = [
-  { color: "#ef4444", bgColor: "#fee2e2" ,textColor:"#E8A7B1",btnColor:"#DE6A9B"},
-  { color: "#3b82f6", bgColor: "#dbeafe" ,textColor:"#7477A6",btnColor:"#4168A3"},
-  { color: "#22c55e", bgColor: "#dcfce7" ,textColor:"#5FA35B",btnColor:"#8AE685"},
-  { color: "#8b5cf6", bgColor: "#ede9fe" ,textColor:"#AF6BB3",btnColor:"#C353C9"},
+  { btncolor: "#DB1818", colors: "#FCE8E8", darkColor:"#F39696"},
+  { btncolor: "#E67129", colors: "#FCF0F8", darkColor:"#F3BA96"},
+  { btncolor: "#E6AD29", colors: "#FCF6E8", darkColor:"#F3D796"},
+  { btncolor: "#A4E629", colors: "#F5FCE8", darkColor:"#D2F396"},
+  { btncolor:"#68E629",colors:"#EFFCE8", darkColor:"#B5F396"},
+  { btncolor:"#29E6D3",colors:"#E8FCFA", darkColor:"#96F3E9"},
+  { btncolor:"#29C0E6",colors:"#E8F8FC", darkColor:"#96E0F3"},
+  { btncolor:"#245AD1",colors:"#E8EFFC", darkColor:"#96B3F3"},
+  { btncolor:"#5529E6",colors:"#EDE8FC", darkColor:"#AC96F3"},
+  { btncolor:"#A129E6",colors:"#F5E8FC", darkColor:"#D196F3"},
+  { btncolor:"#e629ca",colors:"#fce8f9", darkColor:"#F396E5"}
+
 ]
